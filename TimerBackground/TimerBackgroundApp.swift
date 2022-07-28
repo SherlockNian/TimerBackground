090995f3de3b7635d0c1f4edb791e69f73145acf
SherlockNian <githubnian16@gmail.com>
2022-07-28 16:22:05 +0800

    Initial Commit

---
+++ TimerBackground/TimerBackgroundApp.swift
@@ -0,0 +1,18 @@
+//
+//  TimerBackgroundApp.swift
+//  TimerBackground
+//
+//  Created by Hong-Nian Lin
+//
+
+import SwiftUI
+
+@main
+struct TimerBackgroundApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
